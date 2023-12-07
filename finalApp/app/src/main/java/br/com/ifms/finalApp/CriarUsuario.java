b788e0f49decc357b9b8dc2c9e5548250352065c
kauavinicios <kauavinicios0k@gmail.com>
2023-12-06 21:19:05 -0300

    criando telas

---
+++ finalApp/app/src/main/java/br/com/ifms/finalApp/CriarUsuario.java
@@ -0,0 +1,14 @@
+package br.com.ifms.finalApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_usuario);
+    }
+}
--- finalApp/app/src/main/java/br/com/ifms/finalApp/MainActivity.java
+++ finalApp/app/src/main/java/br/com/ifms/finalApp/MainActivity.java
@@ -3,13 +3,20 @@ package br.com.ifms.finalApp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button bt;
     EditText edSenha, edEmail;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
+    TextView criaUL, esqueciSL;
+
+    FirebaseAuth mAuth;
 
     Query query;
 //    ArrayList<Pessoa> pessoaList = new ArrayList<>();
@@ -38,44 +46,72 @@ public class MainActivity extends AppCompatActivity {
         bt = findViewById(R.id.enviarL);
         edEmail = findViewById(R.id.emialL);
         edSenha = findViewById(R.id.senhaL);
+        criaUL = findViewById(R.id.criaUL);
+        esqueciSL = findViewById(R.id.esqueciSL);
 
-        inicializarFirebase();
-        eventoDatabase();
-    }
-    private void inicializarFirebase() {
-        FirebaseApp.initializeApp(MainActivity.this);
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference();
-    }
+        mAuth = FirebaseAuth.getInstance();
 
-    private void limparCampos() {
-        edSenha.setText("");
-        edEmail.setText("");
-    }
-
-    private void eventoDatabase() {
-        databaseReference.child("Pessoa").addValueEventListener(new ValueEventListener() {
+        esqueciSL.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                pessoaList.clear();
-
-                for (DataSnapshot obDataSnapShot: dataSnapshot.getChildren()) {
-                    Pessoa p = obDataSnapShot.getValue(Pessoa.class);
-                    pessoaList.add(p);
-                }
-
-                pessoaArrayAdapter = new ArrayAdapter<>(
-                        MainActivity.this,
-                        android.R.layout.simple_list_item_1,
-                        pessoaList
-                );
-
-                listView.setAdapter(pessoaArrayAdapter);
+            public void onClick(View view) {
+                String email = edEmail.getText().toString();
+                //enviar email para recuperação de senha
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful())
+                            Toast.makeText(MainActivity.this, "Email de recuperação de senha enviado.", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
+        });
 
+        bt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onClick(View view) {
+                String email = edEmail.getText().toString();
+                String senha = edSenha.getText().toString();
+                //login + verifica (e envia) email
+                mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //verificar usuário logado
+                            FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+//                            Intent intent = new Intent(MainActivity.this, TelaPrincipal.class);
+//                            startActivity(intent);
+                            if (usuario.isEmailVerified()) {
+                                 Toast.makeText(MainActivity.this, "Usuário logado.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                 Toast.makeText(MainActivity.this, "Usuário não verificado. Verifique seu e-mail.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "Erro ao logar.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
+        criaUL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edEmail.getText().toString();
+                String senha = edSenha.getText().toString();
+                //cria usuário
+                mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+                            usuario.sendEmailVerification();
+
+                            Toast.makeText(MainActivity.this, "Usuário criado. Verifique seu e-mail.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Usuário NÃO foi criado.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
